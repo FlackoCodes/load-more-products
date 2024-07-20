fac71c8fd34cfaf393d60fc0797b259e9c1c17cb
Flacko <lesliejoe2k2@gmail.com>
2024-07-20 22:45:40 +0000

    new feat: loaded products

--- src/LoadMore.jsx
+++ src/LoadMore.jsx
@@ -3,26 +3,51 @@ import { useState, useEffect } from "react";
 function LoadMore() {
   const [loading, setLoading] = useState(false);
   const [products, setProducts] = useState([]);
-  const [count, setcount] = useState([]);
+  const [count, setcount] = useState(0);
 
-  async function fetchProducts(){
+  async function fetchProducts() {
     try {
-        setLoading(true)
-        const res = await fetch(`https://dummyjson.com/products?limit=20&skip=${count === 0 ? 0: count * 20}`)
-        if (res && res.products && res.products.length) {
-            setProducts(res.products)
-        }
+      setLoading(true);
+      const res = await fetch(
+        `https://dummyjson.com/products?limit=20&skip=${
+          count === 0 ? 0 : count * 20
+        }`
+      );
+      const results = await res.json();
+      if (results && results.products && results.products.length) {
+        setProducts(results.products);
+      }
+      setLoading(false);
     } catch (error) {
-        console.log('error fetching products', error);
+      console.log("error fetching products", error);
+      setLoading(false);
     }
-  }  
+  }
 
-  useEffect(()=>{
-    fetchProducts()
-  },[])
+  useEffect(() => {
+    fetchProducts();
+  }, [count]);
 
-  if (loading) return <div>Loading products ... please wait</div>
-  return <div className="container">LoadMore</div>;
+  if (loading) return <div>Loading products ... please wait</div>;
+  return (
+    <div className="container">
+      <div className="product-container">
+        {products && products.length
+          ? products.map((product) => (
+              <div className="product" key={product.id}>
+                <img src={product.thumbnail} alt={product.description} />
+                <p>{product.title}</p>
+              </div>
+            ))
+          : null}
+      </div>
+      <div className="btn-container">
+        <button onClick={() => setcount(count + 1)} className="btn">
+          Load More products
+        </button>
+      </div>
+    </div>
+  );
 }
 
 export default LoadMore;
