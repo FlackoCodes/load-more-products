5f9d054d7fcc5704c971fb76b68a51ce027874eb
Flacko <lesliejoe2k2@gmail.com>
2024-07-21 19:17:08 +0000

    new feat:prev button

--- src/LoadMore.jsx
+++ src/LoadMore.jsx
@@ -4,7 +4,8 @@ function LoadMore() {
   const [loading, setLoading] = useState(false);
   const [products, setProducts] = useState([]);
   const [count, setcount] = useState(0);
-  const [disabledBtn, setDisabledBtn] = useState(false)
+  const [disableNext, setDisableNext] = useState(false);
+  const [disablePrev, setDisablePrev] = useState(false);
 
   async function fetchProducts() {
     try {
@@ -16,9 +17,12 @@ function LoadMore() {
       );
       const results = await res.json();
       if (results && results.products && results.products.length) {
-        setProducts(results.products);
+        setProducts((prev) => [...prev, ...results.products]);
+        console.log(results);
+        console.log(products);
+        setDisablePrev(count === 0);
+        setLoading(false);
       }
-      setLoading(false);
     } catch (error) {
       console.log("error fetching products", error);
       setLoading(false);
@@ -29,13 +33,21 @@ function LoadMore() {
     fetchProducts();
   }, [count]);
 
+  useEffect(() => {
+    if (products && products.length === 100) setDisableNext(true);
+  }, [products]);
+
+  useEffect(() => {
+    if (products && products.length < 20) setDisablePrev(true);
+  }, [products]);
+
   if (loading) return <div>Loading products ... please wait</div>;
   return (
     <div className="container">
       <div className="product-container">
         {products && products.length
-          ? products.map((product) => (
-              <div className="product" key={product.id}>
+          ? products.map((product, index) => (
+              <div className="product" key={index}>
                 <img src={product.thumbnail} alt={product.description} />
                 <p>{product.title}</p>
               </div>
@@ -43,10 +55,23 @@ function LoadMore() {
           : null}
       </div>
       <div className="btn-container">
-        <button onClick={() => setcount(count + 1)} className="btn">
+        <button
+          disabled={disableNext}
+          onClick={() => setcount(count + 1)}
+          className="btn"
+        >
           Load More products
         </button>
+        {disableNext ? <p>You have reached 100 products</p> : null}
       </div>
+      <button
+        disabled={disablePrev}
+        onClick={() => setcount(count - 1)}
+        className="btn"
+      >
+        Load More products
+      </button>
+      {disablePrev ? <p>Click next btn for more</p> : null}
     </div>
   );
 }
